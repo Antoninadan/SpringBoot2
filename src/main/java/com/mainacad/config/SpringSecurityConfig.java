c26f7706e7cd865372078372f2b0e4a76c89599b
dandy1988 <dandy1988@ukr.net>
2020-02-17 19:28:26 +0200

    stable version

--- src/main/java/com/mainacad/config/SpringSecurityConfig.java
+++ src/main/java/com/mainacad/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.mainacad.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
